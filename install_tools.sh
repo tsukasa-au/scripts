6a21944dd3174df591d877604619921444ca497b
Greg Darke <greg@tsukasa.net.au>
2018-07-08 21:55:39 -0700

    install_tools: Add simple script to install symlinks to tools.

---
+++ install_tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function ln_bin {
+  local BINARY_PATH="${1}"
+  local NEW_SHORT_NAME="${2}"  # May be empty...
+  ln -s "$(readlink --canonicalize "${BINARY_PATH}")" ~/bin/"${NEW_SHORT_NAME}"
+}
+
+ln_bin "$(dirname "$0")/git-tools/git-clone" "gc"
